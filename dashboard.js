603c4c7edd62e035dc32ee27c5606d751bd513d2
Lavanya Arora <lavanyaarora1811@gmail.com>
2025-04-05 04:32:26 +0530

    final dashboard

--- dashboard.js
+++
@@ -1,7 +0,0 @@
-function scrollLeft() {
-    document.getElementById("menu").scrollBy({ left: -220, behavior: 'smooth' });
-}
-
-function scrollRight() {
-    document.getElementById("menu").scrollBy({ left: 220, behavior: 'smooth' });
-}
